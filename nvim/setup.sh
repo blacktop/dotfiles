f44c0330ebd488f80b8753af32039731341f2510
blacktop <blacktop@users.noreply.github.com>
2024-08-19 20:51:04 -0600

    chore(nvim): refactor to use lazy-vim

--- nvim/setup.sh
+++ nvim/setup.sh
@@ -3,18 +3,11 @@ set -o errexit -o nounset
 
 echo "$(gum style --bold --foreground "#6F08B2" " ⇒ ") $(gum style --bold "Setup Neovim")"
 
-VIM_PLUG="$HOME/.vim/autoload/plug.vim"
-
-if [ ! -f "$VIM_PLUG" ]; then
-    echo "$(gum style --bold --foreground "#BE05D0" "  -") Downloading vim-plug..."
-    curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-fi
-
 echo "$(gum style --bold --foreground "#BE05D0" "  -") Setup neovim config..."
 mkdir -p "$HOME/.config/nvim"
-cp $(dirname "$0")/init.vim "$HOME/.config/nvim/init.vim"
+cp -r $(dirname "$0")/init.lua "$HOME/.config/nvim/init.lua"
+cp -r $(dirname "$0")/after "$HOME/.config/nvim/"
+cp -r $(dirname "$0")/lua "$HOME/.config/nvim/"
 
 echo "$(gum style --bold --foreground "#BE05D0" "  -") Update neovim plugins..."
-pip3 install --break-system-packages --user neovim
-nvim +PlugUpdate +PlugUpgrade +qall
+nvim --headless "+Lazy! sync" +qa
