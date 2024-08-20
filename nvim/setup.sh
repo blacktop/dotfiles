aca98fa4c3277cdffccbe0fe97c6f7111d6d4dd4
blacktop <blacktop@users.noreply.github.com>
2024-08-20 17:53:28 -0600

    Update setup.sh

--- nvim/setup.sh
+++ nvim/setup.sh
@@ -4,6 +4,7 @@ set -o errexit -o nounset
 echo "$(gum style --bold --foreground "#6F08B2" " ⇒ ") $(gum style --bold "Setup Neovim")"
 
 echo "$(gum style --bold --foreground "#BE05D0" "  -") Setup neovim config..."
+# http://www.lazyvim.org
 mkdir -p "$HOME/.config/nvim"
 cp -r $(dirname "$0")/* "$HOME/.config/nvim/"
 rm "$HOME/.config/nvim/setup.sh"
