d27e73db9aacfa8bdf1399cfb3b17a351dac1650
blacktop <blacktop@users.noreply.github.com>
2024-08-20 18:40:46 -0600

    chore(nvim): allow bkup of nvim on application

--- nvim/setup.sh
+++ nvim/setup.sh
@@ -1,11 +1,21 @@
 #!/bin/sh
 set -o errexit -o nounset
 
+# NOTE: http://www.lazyvim.org
+
 echo "$(gum style --bold --foreground "#6F08B2" " ⇒ ") $(gum style --bold "Setup Neovim")"
 
 echo "$(gum style --bold --foreground "#BE05D0" "  -") Setup neovim config..."
-# http://www.lazyvim.org
+# required
+mv ~/.config/nvim{,.bak}
+
+# optional but recommended
+mv ~/.local/share/nvim{,.bak}
+mv ~/.local/state/nvim{,.bak}
+mv ~/.cache/nvim{,.bak}
+
 mkdir -p "$HOME/.config/nvim"
+
 cp -r $(dirname "$0")/* "$HOME/.config/nvim/"
 rm "$HOME/.config/nvim/setup.sh"
 
