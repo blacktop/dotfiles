d8bbd5ff539ae5b184b7b5efc9e126b2880a0a77
blacktop <blacktop@users.noreply.github.com>
2024-08-24 12:40:37 -0600

    Update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -91,6 +91,7 @@ set -Ux FZF_DEFAULT_OPTS "\
 # --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
 # --color=marker:#b4befe,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
 # --color=selected-bg:#45475a \
+# --border thinblock \
 # --multi"
 
 # zoxide
