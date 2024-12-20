72dc457d8e678a5476b3d60458165076d00f4a58
blacktop <blacktop@users.noreply.github.com>
2024-12-20 16:19:03 -0700

    Update setup.sh

--- git/setup.sh
+++ git/setup.sh
@@ -7,6 +7,6 @@ git config --global core.editor "code -w -n"
 git config --global pull.rebase true
 git config --global rebase.autoStash true
 
-echo "$(gum style --bold --foreground "#BE05D0" "  -") Install gh-dash..."
-gh extension install dlvhdr/gh-dash
-cp -r $(dirname "$0")/gh-dash/* "$HOME/.config/gh-dash/"
+# echo "$(gum style --bold --foreground "#BE05D0" "  -") Install gh-dash..."
+# gh extension install dlvhdr/gh-dash
+# cp -r $(dirname "$0")/gh-dash/* "$HOME/.config/gh-dash/"
