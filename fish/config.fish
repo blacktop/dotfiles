1e50bdaa6148bcf8698b2590a003eb20550746c6
blacktop <blacktop@users.noreply.github.com>
2025-02-04 21:03:10 -0700

    fix: fish

--- fish/config.fish
+++ fish/config.fish
@@ -1,28 +1,26 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+fish_add_path -a $HOME/Library/Python/3.13/bin
+fish_add_path -a $HOME/go/bin
+fish_add_path -a /opt/homebrew/bin
+fish_add_path -a /opt/homebrew/sbin
+fish_add_path -a /opt/homebrew/opt/openjdk/bin
+fish_add_path -a /opt/homebrew/opt/ruby/bin
+fish_add_path -a $HOME/.cargo/bin
+# fish_add_path -a $HOME/.modular/pkg/packages.modular.com_mojo/bin
+
 if test -e /opt/homebrew/bin/brew
     eval $(/opt/homebrew/bin/brew shellenv)
 end
-
-set -xg EDITOR (which code) -w
-
-function fish_user_key_bindings
-    set -U FZF_LEGACY_KEYBINDINGS 0
-    source $HOME/.config/fish/conf.d/fzf_key_bindings.fish
-    source $HOME/.config/fish/functions/keys_bindings.fish
-end
-
-set -x FZF_COMPLETE 1
-set -x FZF_REVERSE_ISEARCH_OPTS '--preview-window=up:10 --preview="echo {}" --height 100%'
 
 # locals.fish is a home for anything machine specific
 if test -e ~/.config/fish/locals.fish
     source ~/.config/fish/locals.fish
 end
 
-fish_add_path -a $HOME/Library/Python/3.10/bin
-fish_add_path -a $HOME/go/bin
-fish_add_path -a /opt/homebrew/opt/openjdk/bin
-fish_add_path -a /opt/homebrew/opt/ruby/bin
-fish_add_path -a $HOME/.cargo/bin
+set -xg EDITOR (which code) -w
 
 # STYLE #########################################
 set fish_greeting
@@ -35,10 +33,11 @@ starship init fish | source
 enable_transience
 
 # alias #########################################
-# tmux
+alias l 'eza -l -g --git'
+# Shows all timestamps in their full glory
+alias lf 'eza -guUmhl --git --time-style long-iso'
+alias lt 'eza -guUmhl -T --hyperlink --git --time-style long-iso'
 alias ta 'tmux new -A -s default'
-# git AI
-alias gcai 'git --no-pager diff | mods 'write a commit message for this patch. also write the long commit message. use semantic commits. break the lines at 80 chars' >.git/gcai; git commit -a -F .git/gcai -e'
 
 # homebrew
 set -x HOMEBREW_CASK_OPTS '--appdir=/Applications --fontdir=~/Library/Fonts --require-sha'
@@ -60,6 +59,15 @@ set -x  MANPAGER 'less -X'
 set -x GPG_TTY $(tty);
 
 # fzf settings
+function fish_user_key_bindings
+    set -U FZF_LEGACY_KEYBINDINGS 0
+    source $HOME/.config/fish/conf.d/fzf_key_bindings.fish
+    source $HOME/.config/fish/functions/keys_bindings.fish
+end
+
+set -x FZF_COMPLETE 1
+set -x FZF_REVERSE_ISEARCH_OPTS '--preview-window=up:10 --preview="echo {}" --height 100%'
+
 set -x FZF_COMPLETE 1
 set -x FZF_REVERSE_ISEARCH_OPTS '--preview-window=up:10 --preview="echo {}" --height 100%'
 set -x FZF_LEGACY_KEYBINDINGS 0
