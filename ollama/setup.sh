33799a2bb16d54fd82c2407b690c68f342eb5930
blacktop <blacktop@users.noreply.github.com>
2025-02-04 20:47:17 -0700

    Update setup.sh

--- ollama/setup.sh
+++ ollama/setup.sh
@@ -1,9 +1,15 @@
 #!/bin/sh
 set -o errexit -o nounset
 
+# Start a ollama process
+ollama serve &
+PID=$!
+# Ensure cleanup on exit
+trap "kill $PID" EXIT
+
 echo "$(gum style --bold --foreground "#BE05D0" "  -") Download Ollama Models..."
 ollama pull deepseek-r1:14b
 ollama pull llama3.2-vision:latest
 ollama pull llama3.2:latest
 ollama pull mistral-small:24b
-ollama run codellama:13b
+# ollama run codellama:13b
