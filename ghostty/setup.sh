f89bb2f56e31457f76b075d6b85ca4e7aefb8971
blacktop <blacktop@users.noreply.github.com>
2025-02-04 19:41:00 -0700

    chore: update Brewfile

--- ghostty/setup.sh
+++ ghostty/setup.sh
@@ -3,7 +3,7 @@ set -o errexit -o nounset
 
 echo "$(gum style --bold --foreground "#6F08B2" " ⇒ ") $(gum style --bold "Setup Ghostty")"
 
-brew install --cask ghostty
+brew install --cask ghostty@tip
 
 mkdir -p "$HOME/.config/ghostty"
 cp $(dirname "$0")/config "$HOME/.config/ghostty/config"
