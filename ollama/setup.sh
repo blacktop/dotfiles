c9574ad2b1a6df3c492d1e387eff28dba6eb467d
blacktop <blacktop@users.noreply.github.com>
2025-05-31 21:21:10 -0600

    Update setup.sh

--- ollama/setup.sh
+++ ollama/setup.sh
@@ -9,7 +9,6 @@ trap "kill $PID" EXIT
 
 echo "$(gum style --bold --foreground "#BE05D0" "  -") Download Ollama Models..."
 ollama pull deepseek-r1:14b
-ollama pull llama3.2-vision:latest
-ollama pull llama3.2:latest
-ollama pull mistral-small:24b
-# ollama run codellama:13b
+ollama pull gemma3:12b
+ollama pull qwen3:14b
+ollama pull devstral:24b
