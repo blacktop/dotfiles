3232934a5f1807bd2815f7ecf585feaa82704675
blacktop <blacktop@users.noreply.github.com>
2025-11-19 23:14:34 -0700

    Update lms.sh

--- lmstudio/lms.sh
+++ lmstudio/lms.sh
@@ -6,7 +6,7 @@ echo "$(gum style --bold --foreground "#6F08B2" " ⇒") $(gum style --bold "Setu
 brew install --cask lm-studio
 
 echo "$(gum style --bold --foreground "#BE05D0" "  -") eDownload LM Studio Models..."
-lms get --yes --mlx openai/gpt-oss-20b
-lms get --yes --mlx qwen/qwen3-coder-30b
-lms get --yes --mlx gemma3:12b
-lms get --yes --mlx bytedance/seed-oss-36b
+lms get --yes openai/gpt-oss-20b
+lms get --yes qwen/qwen3-coder-30b
+lms get --yes google/gemma-3-27b
+lms get --yes bytedance/seed-oss-36b
