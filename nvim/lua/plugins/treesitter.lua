65bfdc673630dd3997eb5b5e1c2a0cd45ff5c836
blacktop <blacktop@users.noreply.github.com>
2026-03-30 18:52:33 -0600

    Update treesitter.lua

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,10 +1,10 @@
 return {
     {
         "nvim-treesitter/nvim-treesitter",
-        opts = {
-            ensure_installed = {
+        opts = function(_, opts)
+            vim.list_extend(opts.ensure_installed or {}, {
                 "fish",
-            },
-        },
+            })
+        end,
     },
 }
